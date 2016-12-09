25a87b97c522987e07f0226e3ba40b04fdfc9682
DC* <des@riseup.net>
2016-12-08 23:26:42 -0300

    Add enable/disable flag

--- plugin.zsh
+++ plugin.zsh
@@ -1,10 +1,15 @@
+# Enable or disable this plugin
+PROMPT_GEOMETRY_DIR_INFO=${PROMPT_GEOMETRY_DIR_INFO:-true}
+
+# Return true or false to enable/disable
 geometry_prompt_dir_info_setup() {
-
-
+  return $PROMPT_GEOMETRY_DIR_INFO
 }
 
+# Render directory information (file count, size)
 geometry_prompt_dir_info_render() {
-    echo "::" $(/bin/ls -1 | /usr/bin/wc -l | /bin/sed 's: ::g') files, $(/bin/ls -lah | /bin/grep -m 1 total | /bin/sed 's/total //')
+  echo "::" $(/bin/ls -1 | /usr/bin/wc -l | /bin/sed 's: ::g') files, $(/bin/ls -lah | /bin/grep -m 1 total | /bin/sed 's/total //')
 }
 
 geometry_plugin_register dir_info
+
